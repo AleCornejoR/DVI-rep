9a5c9fdec4ce902744c9c79449fb9e72bb78c677
Alejandro Cornejo <rafitttardz@hotmail.com>
2024-03-27 23:02:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # main.py
-from source.view import Interface
-from PyQt6.QtWidgets import QApplication
+"""
+Módulo principal que inicia la aplicación.
+"""
+
+#-----------------------------------------
+# LIBRERIAS
+#-----------------------------------------
+
+from source.view import Interface  # Importa la clase Interface desde view.py
+from PyQt6.QtWidgets import QApplication  # Importa la clase QApplication desde PyQt6.QtWidgets
+
+#-----------------------------------------
+# CICLO PRINCIPAL
+#-----------------------------------------
 
 def main():
-    app = QApplication([])
-    interface = Interface()
-    interface.show()
-    app.exec()
+    app = QApplication([])  # Crea una instancia de la aplicación QApplication
+    interface = Interface()  # Crea una instancia de la interfaz
+    interface.show()  # Muestra la interfaz
+    app.exec()  # Ejecuta la aplicación
 
 if __name__ == "__main__":
-    main()
+    main()  # Llama a la función principal si el script es ejecutado directamente
