1e75b2abd024a7e2c08ec2b7f9b9df64d3e79328
Alejandro Cornejo <rafitttardz@hotmail.com>
2024-03-27 23:04:34 -0600

    Update model.py

--- source/model.py
+++ source/model.py
@@ -1,7 +1,18 @@
 # source/model.py
+"""
+Módulo que define el modelo en el patrón Modelo-Vista-Controlador (MVC).
+El modelo representa los datos y la lógica de la aplicación.
+"""
+#-----------------------------------------
+# CLASES PARA MODELO MODEL.PY
+#-----------------------------------------
 class Model:
     def __init__(self):
+        # Constructor de la clase Model.
         pass
 
-    def print_consecutivo(self, consecutivo_value):
-        print("Searching for consecutivo value:", consecutivo_value)
+    def print_consecutivo(self, consecutive_value): # Método para imprimir el valor consecutivo.
+        print("Searching for consecutive value:", consecutive_value)
+
+    def print_sap(self, sap_value): # Método para imprimir el valor de SAP.
+        print("Searching for sap value:", sap_value)
